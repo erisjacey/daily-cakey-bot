20cfcab546a84369d83193f4b62d694b0dadbbe1
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-20 12:30:16 +0800

    Refine caption

--- __main__.py
+++ __main__.py
@@ -6,6 +6,7 @@ import re
 TOKEN = '1873304340:AAGpY3F5leYN_y3Ebz-mkvDf6o7Zh35Riic'
 API_KEY = '0664cb0dac0948c5a70e52eb532f45fe'
 REQUEST_URL = 'https://api.spoonacular.com/recipes/random?number=1&tags=dessert&apiKey={}'
+MAX_SUMMARY_CHAR_LENGTH = 1200
 
 
 def get_recipe():
@@ -16,19 +17,35 @@ def get_recipe():
 def get_image_url_summary():
     allowed_extension = ['jpg', 'jpeg', 'png']
     file_extension = ''
-    while file_extension not in allowed_extension:
+    summary_char_length = 2000
+    while file_extension not in allowed_extension or summary_char_length > MAX_SUMMARY_CHAR_LENGTH:
         recipe = get_recipe()
-        url = recipe['image']
-        summary = recipe['summary']
-        file_extension = re.search("([^.]*)$", url).group(1).lower()
-    return (url, summary)
+        print('Getting recipe...')
+        try:
+            name = recipe['title']
+            image_url = recipe['image']
+            source_url = recipe['sourceUrl']
+            summary = recipe['summary']
+            summary_char_length = len(summary)
+            print('Summary character length:', summary_char_length)
+        except KeyError:
+            continue
+        file_extension = re.search("([^.]*)$", image_url).group(1).lower()
+    return (name, image_url, source_url, summary)
+
+
+def construct_caption(name, src_url, summary):
+    return '<b>{}</b> \
+            \n<a href=\"{}\"><b>Recipe here</b></a> \
+            \n<b>Description</b>: {}'.format(name, src_url, summary)
 
 
 def pls(update, context):
-    (url, summary) = get_image_url_summary()
+    (name, image_url, source_url, summary) = get_image_url_summary()
+    caption = construct_caption(name, source_url, summary)
     chat_id = update.message.chat_id
-    context.bot.send_photo(chat_id=chat_id, photo=url,
-                           caption=summary, parse_mode=ParseMode.HTML)
+    context.bot.send_photo(chat_id=chat_id, photo=image_url,
+                           caption=caption, parse_mode=ParseMode.HTML)
 
 
 def main():
