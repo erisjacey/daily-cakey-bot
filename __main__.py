01904f61696d0bb83de82e8d4628093ed17d595c
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-20 11:12:59 +0800

    Add exception handling

--- __main__.py
+++ __main__.py
@@ -4,17 +4,27 @@ import re
 
 TOKEN = '1873304340:AAGpY3F5leYN_y3Ebz-mkvDf6o7Zh35Riic'
 
+
 def get_url():
-    contents = requests.get('https://random.dog/woof.json').json()    
-    # print(contents)
+    contents = requests.get('https://random.dog/woof.json').json()
     url = contents['url']
     return url
 
+
+def get_image_url():
+    allowed_extension = ['jpg', 'jpeg', 'png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$", url).group(1).lower()
+    return url
+
+
 def bop(update, context):
-    url = get_url()
-    # print(url)
+    url = get_image_url()
     chat_id = update.message.chat_id
     context.bot.send_photo(chat_id=chat_id, photo=url)
+
 
 def main():
     updater = Updater(TOKEN)
@@ -22,6 +32,7 @@ def main():
     dp.add_handler(CommandHandler('bop', bop))
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
