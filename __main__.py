f8cfa7a3a4d47402da33d712b9c061759aab11cf
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-21 13:26:39 +0800

    Misc fixes

--- __main__.py
+++ __main__.py
@@ -99,8 +99,6 @@ def main():
 
     # Log all errors
     dp.add_error_handler(error)
-    updater.start_polling()
-    updater.idle()
 
     # Start the Bot 
     updater.start_webhook(listen="0.0.0.0",
