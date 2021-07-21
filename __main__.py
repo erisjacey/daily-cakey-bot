d472d5a257fa55acb7b73731fe74250f8a74f82e
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-21 12:15:57 +0800

    Add docstrings and start command

--- __main__.py
+++ __main__.py
@@ -17,12 +17,23 @@ REQUEST_URL = 'https://api.spoonacular.com/recipes/random?number=1&tags=dessert&
 MAX_SUMMARY_CHAR_LENGTH = 1200
 
 
+def start(update, context):
+    """Sends a message when the command /start is issued."""
+    update.message.reply_text(
+        'Hi! Enter /pls to obtain a random dessert recipe.')
+
+
 def get_recipe():
+    """Makes an API call to Spoonacular to retrieve exactly one random recipe."""
     contents = requests.get(REQUEST_URL.format(API_KEY)).json()
     return contents['recipes'][0]
 
 
-def get_image_url_summary():
+def get_recipe_details():
+    """
+    Obtains the details of a random recipe selected.
+    Returns its name, image URL, source (recipe) URL, and summary description.
+    """
     allowed_extension = ['jpg', 'jpeg', 'png']
     file_extension = ''
     summary_char_length = 2000
@@ -43,17 +54,21 @@ def get_image_url_summary():
 
 
 def construct_caption(name, src_url, summary):
+    """Creates a caption based on a selected recipe into HTML format."""
     return '<b>{}</b> \
             \n<a href=\"{}\"><b>Recipe here</b></a> \
             \n<b>Description</b>: {}'.format(name, src_url, summary)
 
 
 def error(update, context):
+    """Logs errors and sends error messages caused by updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+    context.bot.send_message('There seems to be an error somewhere. Sorry :(')
 
 
 def pls(update, context):
-    (name, image_url, source_url, summary) = get_image_url_summary()
+    """Sends a randomly-selected recipe of a dessert with relevant details when the command /pls is issued."""
+    (name, image_url, source_url, summary) = get_recipe_details()
     caption = construct_caption(name, source_url, summary)
     chat_id = update.message.chat_id
     context.bot.send_photo(chat_id=chat_id, photo=image_url,
@@ -68,6 +83,7 @@ def main():
     dp = updater.dispatcher
 
     # On different commands
+    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler('pls', pls))
 
     # Log all errors
