245450aa5ae57389b76c4834e7c325770a96a2ac
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-21 13:37:20 +0800

    Change web hook call

--- __main__.py
+++ __main__.py
@@ -103,8 +103,8 @@ def main():
     # Start the Bot 
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook(WEBAPP_URL + TOKEN)
+                          url_path=TOKEN,
+                          webhook_url=WEBAPP_URL+TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
