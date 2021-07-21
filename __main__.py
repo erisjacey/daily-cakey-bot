68a40e57354f57b63fcb3e4bfbc9b2a6dffde541
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2021-07-21 11:27:41 +0800

    Update code for deployment

--- __main__.py
+++ __main__.py
@@ -3,15 +3,21 @@ from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler
 import requests
 import re
+import os
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+PORT = int(os.environ.get('PORT', 5000))
+
 TOKEN = '1873304340:AAGpY3F5leYN_y3Ebz-mkvDf6o7Zh35Riic'
 API_KEY = '0664cb0dac0948c5a70e52eb532f45fe'
 REQUEST_URL = 'https://api.spoonacular.com/recipes/random?number=1&tags=dessert&apiKey={}'
 MAX_SUMMARY_CHAR_LENGTH = 1200
+
+WEBAPP_URL = 'https://daily-cakey-bot.herokuapp.com/'
 
 
 def get_recipe():
@@ -58,11 +64,29 @@ def pls(update, context):
 
 
 def main():
+    # Create the Updater and pass it to the bot's token
     updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
+
+    # On different commands
     dp.add_handler(CommandHandler('pls', pls))
+
+    # Log all errors
     dp.add_error_handler(error)
     updater.start_polling()
+    updater.idle()
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook(WEBAPP_URL + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
